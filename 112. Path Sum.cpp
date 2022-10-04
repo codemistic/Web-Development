a86e82447de65722856e03672e4e0ad3a0545078
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2022-10-04 23:35:28 +0530

    Delete 112. Path Sum.cpp

--- 112. Path Sum.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool hasPathSum(TreeNode* root, int sum) {
-        if(!root) return false;
-        if(!root->left && !root->right && root->val == sum) return true;
-        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
-    }
-};
