46c0cc243835c6b6ace2cd37ab33b96ae002a407
Aarya Chopkar <aaryachopkar@gmail.com>
2022-10-04 11:46:12 +0530

    Added a awesome landing page project

---
+++ Awesome Landing Page/script.js
@@ -0,0 +1,8 @@
+function imgSlider(any){
+    document.querySelector(".amul").src = any;
+}
+
+function changeColour(colour){
+    const circle = document.querySelector(".circle");
+    circle.style.background = colour;
+}
