b84ce9830d29765972e32643b1e275e5214bc632
Vatsalya Sharma <21bee025@nith.ac.in>
2022-10-05 22:58:59 +0530

    Added display message depending upon the BMI calculated.

--- BMI Calculator/index.js
+++ BMI Calculator/index.js
@@ -8,4 +8,11 @@ function getbmivalue(){
     var newbmivalue = weight/Math.pow(height,2);
     newbmivalue=Math.round(newbmivalue);
     document.getElementById('bmivalue').value=newbmivalue;
+    if(newbmivalue >= 25){
+      document.getElementById('healthStatus').textContent = "You are overweight!";
+    } else if (newbmivalue <= 19) {
+      document.getElementById('healthStatus').textContent = "You are underweight!";
+    } else {
+      document.getElementById('healthStatus').textContent = "Your body is healthy and active!";
+    }
 }
