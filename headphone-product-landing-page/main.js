64c96c87074bcae4d0bdf576226649bce126c21f
Dimple choudhary <96401557+Dimple-Choudhary@users.noreply.github.com>
2022-10-07 13:38:30 +0530

    Update main.js

--- headphone-product-landing-page/main.js
+++ headphone-product-landing-page/main.js
@@ -5,25 +5,10 @@ $(document).ready(function(){
        $('.navbar').toggleClass('nav-toggle');
    });
 
-   $(window).on('load scroll',function(){
+//    $(window).on('load scroll',function(){
 
-       $('#menu').removeClass('fa-times');
-       $('.navbar').removeClass('nav-toggle');
-
-       $('section').each(function(){
-
-           let top = $(window).scrollTop();
-           let height = $(this).height();
-           let offset = $(this).offset().top - 200;
-           let id = $(this).attr('id');
-
-           if(top > offset && top < offset + height){
-               $('.navbar ul li a').removeClass('active');
-               $('.navbar').find(`[href="#${id}"]`).addClass('active');
-           }
-
-       });
-
-   });
+//        $('#menu').removeClass('fa-times');
+//        $('.navbar').removeClass('nav-toggle');
+//    });
 
 });
