1e13c3480d864828cf52f30074b5c5bbefffe1e0
divyaa1511 <102688183+divyaa1511@users.noreply.github.com>
2022-10-08 18:13:30 +0530

    Update settings.py

--- email verification/src/src/settings.py
+++ email verification/src/src/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dimstar1212@gmail.com'
-EMAIL_HOST_PASSWORD = 'fsnlpdbkidbzchvt'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
