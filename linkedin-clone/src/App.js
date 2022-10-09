401efbf79e7ee0aa91d0e1ccf3ad4629ecede940
Himanshu Sharma <97340980+Robo707@users.noreply.github.com>
2022-10-09 10:20:12 +0530

    linkedin clone using react and redux

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from './features/userSlice';
+import Login from './Login';
+import { auth } from './firebase';
+import Widgets from './Widgets';
+
+
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          Login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        )
+      } else {
+        dispatch(logout());
+      }
+    })
+  }, )
+
+  return (
+    <div className="app">
+      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app__body'>
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
