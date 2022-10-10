bc4a3b39ca5818e41b02b10253c62e010baddb72
Ayush Kanduri <76626529+Ayush-Kanduri@users.noreply.github.com>
2022-10-10 18:36:46 +0530

    Add Project: Mini Ipod App using ReactJS

---
+++ Mini Ipod App using ReactJS/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/index.css";
+import App from "./components/stateful/App";
+
+// Rendering the React App into the HTML DOM Element "root"
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
