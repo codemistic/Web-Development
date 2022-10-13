6f82990b1c8d1fe1fa4657a9ee3fa52dfa9a5f0f
Anupesh Kumar Verma <77433959+anupeshverma@users.noreply.github.com>
2022-10-14 00:10:52 +0530

    Feature for valid height and weight
    Feature for valid height and weight wasn't working correctly so fix that and add an extra feature for both valid height and weight

--- BMI Calculator/index.js
+++ BMI Calculator/index.js
@@ -1,26 +1,28 @@
-function getbmivalue(){
+function getbmivalue() {
     var weight = document.getElementById('weight').value;
     var height = document.getElementById('height').value;
 
-    height=height*12;
-    height=height*0.025; //Now height is in meter
+    height = height * 12;
+    height = height * 0.025; //Now height is in meter
 
-    var newbmivalue = weight/Math.pow(height,2);
-    newbmivalue=Math.round(newbmivalue);
-    document.getElementById('bmivalue').value=newbmivalue;
+    var newbmivalue = weight / Math.pow(height, 2);
+    newbmivalue = Math.round(newbmivalue);
+    document.getElementById('bmivalue').value = newbmivalue;
     // checking for invalid inputs
-    if (height=== "" || isNan(height))
+    if(height == "" && weight == "")
+        document.getElementById('healthStatus').textContent = "Enter valid Height and Weight";
+    else if (height == "" || isNaN(height))
         document.getElementById('healthStatus').textContent = "Enter a valid Height";
-    else if (weight=== "" || isNan(weight))
+    else if (weight == "" || isNaN(weight))
         document.getElementById('healthStatus').textContent = "Enter a valid Weight";
     // if both inputs are valid then calculating bmi
-    else{
-    if(newbmivalue >= 25){
-      document.getElementById('healthStatus').textContent = "You are overweight!";
-    } else if (newbmivalue <= 19) {
-      document.getElementById('healthStatus').textContent = "You are underweight!";
-    } else {
-      document.getElementById('healthStatus').textContent = "Your body is healthy and active!";
-    }
+    else {
+        if (newbmivalue >= 25) {
+            document.getElementById('healthStatus').textContent = "You are overweight!";
+        } else if (newbmivalue <= 19) {
+            document.getElementById('healthStatus').textContent = "You are underweight!";
+        } else {
+            document.getElementById('healthStatus').textContent = "Your body is healthy and active!";
+        }
     }
 }
