ea29662c1ce1e0c62f45054df2ce3b30db5afc35
Jason Bruno Dsouza <106374595+Jsn99@users.noreply.github.com>
2022-11-09 19:41:22 +0300

    let and const added
    In function getRandomColor() let and const is more suitable than var.

--- Counter/Counter.js
+++ Counter/Counter.js
@@ -20,9 +20,9 @@ $('input').on('mouseover click', function () {
 });
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
