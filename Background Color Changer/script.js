0650fec05669607eec283411a7f4b0990d32dfc3
Aleksei__developer <84816879+AlekseidDEV@users.noreply.github.com>
2023-09-18 21:55:38 +0300

    Update script.js
    Previously, this button would stop at the last color and no longer work. Now there is a condition that allows you to change colors indefinitely.

--- Background Color Changer/script.js
+++ Background Color Changer/script.js
@@ -1,6 +1,31 @@
-var color= ["#222f3e","#f368e0","#ee5353","#6c55cc","#24ffff","#ff2626","#ffffff","#000000","#7878ff","#cdcdcd","#454545","#909090","#cccddd","#0abde3","#10ac84","#5f27cd"];
-var i=0;
-document.querySelector("button").addEventListener("click",function(){
-	i=1<color.length ? ++i : 0;
-	document.querySelector("body").style.background=color[i]
-})
+const btn = document.querySelector("button");
+
+let color = [
+  "#222f3e",
+  "#f368e0",
+  "#ee5353",
+  "#6c55cc",
+  "#24ffff",
+  "#ff2626",
+  "#ffffff",
+  "#000000",
+  "#7878ff",
+  "#cdcdcd",
+  "#454545",
+  "#909090",
+  "#cccddd",
+  "#0abde3",
+  "#10ac84",
+  "#5f27cd",
+];
+let i = 0;
+
+btn.addEventListener("click", () => {
+  if (i <= color.length - 2) {
+    i++;
+    document.body.style.background = `${color[i]}`;
+  } else {
+    i = 0;
+    document.body.style.background = `${color[i]}`;
+  }
+});
